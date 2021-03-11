e0b1a8bf79bbc5e7342287f7ddba0f8b8d111728
54Satoru <satoru.pro.54@gmail.com>
2021-03-11 19:18:47 +0900

    AWS修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = '54satoruawss3'
+  config.fog_directory  = 'AWS_BUCKET'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AKIAQF33BSMME7BFTXR6'],
-    aws_secret_access_key: ENV['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'],
-    region: ENV['ap-northeast-1'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+    region: ENV['AWS_REGION'],
     path_style: true
   }
 
