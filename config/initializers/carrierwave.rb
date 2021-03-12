e5aa1b5d075f23a065ce02a6b43f3c83824c68d2
54Satoru <satoru.pro.54@gmail.com>
2021-03-12 18:10:31 +0900

    S3heroku設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_directory  = 'AWS_BUCKET'
+  config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/54satoru-buket"
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   config.fog_credentials = {
     provider: 'AWS',
