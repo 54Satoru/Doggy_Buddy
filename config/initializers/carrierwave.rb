cc5091e597acd681edb7497c38ab451c64211c42
54Satoru <satoru.pro.54@gmail.com>
2021-03-12 16:53:33 +0900

    S3heroku設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_directory  = 'AWS_BUCKET'
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY'],
@@ -15,3 +16,4 @@ CarrierWave.configure do |config|
   }
 
 end
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
